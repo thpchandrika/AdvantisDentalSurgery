f3652a212998c87b9cf469308b38436268d1b3f3
thpchandrika <thpchandrika@gmail.com>
2024-04-26 20:41:18 -0500

    aws rds database connection

--- src/test/java/cs489/apsd/dentalsurgeryapp/DentistControllerTests.java
+++ src/test/java/cs489/apsd/dentalsurgeryapp/DentistControllerTests.java
@@ -2,11 +2,18 @@ package cs489.apsd.dentalsurgeryapp;
 
 import cs489.apsd.dentalsurgeryapp.controller.AddressController;
 import cs489.apsd.dentalsurgeryapp.domain.Address;
+import cs489.apsd.dentalsurgeryapp.domain.Dentist;
+import cs489.apsd.dentalsurgeryapp.dto.dentist.DentistAdapter;
+import cs489.apsd.dentalsurgeryapp.service.AddressService;
+import cs489.apsd.dentalsurgeryapp.service.DentistService;
+import cs489.apsd.dentalsurgeryapp.service.impl.AdsUserDetailsService;
+import cs489.apsd.dentalsurgeryapp.util.JwtUtilityService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,18 +34,31 @@ public class DentistControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void testGetAddressList() throws Exception {
-        List<Address> addresses = new ArrayList<>();
-        addresses.add(new Address(1,"123 Street", "45655", "Fairfield", "IA"));
-        addresses.add(new Address(2,"456 Avenue", "65445", "Euless", "TX"));
+    @MockBean
+    private DentistService dentistService;
 
-        mockMvc.perform(get("/adsweb/api/v1/addresses/"))
+    @MockBean
+    private AddressService addressService;
+
+    @MockBean
+    private JwtUtilityService jwtUtilityService;
+
+    @MockBean
+    private AdsUserDetailsService adsUserDetailsService;
+
+    @Test
+    public void testGetDentistList() throws Exception {
+        List<Dentist> dentists = new ArrayList<>();
+        dentists.add(new Dentist("D001","John", "Doe", "1234567890", "john.doe@example.com", "General Dentistry"));
+        dentists.add(new Dentist("D002","Jane", "Smith", "9876543210", "jane.smith@example.com", "Orthodontics"));
+        when(dentistService.getAllDentist()).thenReturn(DentistAdapter.getDentistResponseList(dentists));
+
+        mockMvc.perform(get("/adsweb/api/v1/dentists/"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isSuccess").value(true))
                 .andExpect(jsonPath("$.data", hasSize(2)))
-                .andExpect(jsonPath("$.data[0].street").value("123 Street"))
-                .andExpect(jsonPath("$.data[1].city").value("Euless"));
+                .andExpect(jsonPath("$.data[0].dentistID").value("D001"))
+                .andExpect(jsonPath("$.data[1].specialization").value("Orthodontics"));
     }
 
 }
